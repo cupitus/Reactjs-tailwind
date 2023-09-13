bdc1f6bef297720354c9e76776217040b3fe0677
cupitus <89416133+cupitus@users.noreply.github.com>
2023-09-13 14:00:05 +0100

    building the components

--- website_app/src/App.jsx
+++ website_app/src/App.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import style from './style'
+import {Navbar,Billing,CardDeal,Business,Clients,CTA,Stats,Footer,Testimonials, Hero,} from './Components'
+
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}>
+          <Navbar/>
+        </div>
+      </div>
 
-    </div>
+      <div className={`bg-primary ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Stats/>
+          <Business/>
+          <Billing/>
+          <CardDeal/>
+          <Testimonials/>
+          <Clients/>
+          <CTA/>
+          <Footer/>
+        </div>
+      </div>
+    </div >
+
   )
 }
 
---
+++ website_app/src/style.js
@@ -0,0 +1,52 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
+style.js
+const styles = {
+  boxWidth: "xl:max-w-[1280px] w-full",
+
+  heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+  paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
+
+  flexCenter: "flex justify-center items-center",
+  flexStart: "flex justify-center items-start",
+
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-12 py-4",
+
+  marginX: "sm:mx-16 mx-6",
+  marginY: "sm:my-16 my-6",
+};
+
+export const layout = {
+  section: `flex md:flex-row flex-col ${styles.paddingY}`,
+  sectionReverse: `flex md:flex-row flex-col-reverse ${styles.paddingY}`,
+
+  sectionImgReverse: `flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
+  sectionImg: `flex-1 flex ${styles.flexCenter} md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+
+  sectionInfo: `flex-1 ${styles.flexStart} flex-col`,
+};
+
+export default styles;
